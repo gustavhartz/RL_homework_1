4e635c0fed17f7d50232c1f4585d4bdd6458d4ac
Gustav Hartz <gustav.s.hartz@gmail.com>
2022-10-11 23:13:39 -0700

    Decrease logging to one in 500

--- hw3/cs285/infrastructure/rl_trainer.py
+++ hw3/cs285/infrastructure/rl_trainer.py
@@ -335,7 +335,9 @@ class RL_Trainer(object):
         return paths, envsteps_this_batch, train_video_paths
 
     def train_agent(self):
-        print('\nTraining agent using sampled data from replay buffer...')
+        # Decrease logging a bit
+        if np.random.randint(500) == 0:
+            print('\nTraining agent using sampled data from replay buffer...')
         all_logs = []
         for train_step in range(self.params['num_agent_train_steps_per_iter']):
 
