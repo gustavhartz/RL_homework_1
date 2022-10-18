0c8bc7a5440438f7690e6536b74397b63eaaf6c2
Gustav Hartz <gustav.s.hartz@gmail.com>
2022-10-17 19:46:40 -0700

    SAC should be working

--- hw3/cs285/policies/sac_policy.py
+++ hw3/cs285/policies/sac_policy.py
@@ -46,6 +46,11 @@ class MLPPolicySAC(MLPPolicy):
     def get_action(self, obs: np.ndarray, sample=True) -> np.ndarray:
         # TODO: return sample from distribution if sampling
         # if not sampling return the mean of the distribution
+        if len(obs.shape) > 1:
+            obs = obs
+        else:
+            obs = obs[None]
+
         distribution = self(ptu.from_numpy(obs))
 
         if sample:
